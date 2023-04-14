deee9330738677782232d84769d36c83d345a266
404ibra <ibra.aktass@gmail.com>
2023-04-14 18:42:46 +0300

    main page sliding images area is completed

--- Amazon Clone/App/MainView.swift
+++ Amazon Clone/App/MainView.swift
@@ -7,6 +7,51 @@
 
 import SwiftUI
 
+
+
+
+struct HomeView: View {
+    @EnvironmentObject var mainVM: MainVM
+    @State var slidingImages: Int = 0
+    //Props
+    
+    //Body
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center, spacing: 0) {
+                MainHeaderView()
+                  //  .frame(height:125)
+                LocationArea()
+                if mainVM.isSearchEditing {
+                    SearchSuggestView().opacity(mainVM.isSearchEditing ? 1 : 0)
+                }else{
+                    TabView(selection: $slidingImages) {
+                        ForEach(0...4, id: \.self){i in
+                            Image("sliding_img_\(i)")
+                                .resizable()
+                                .tag(i)
+                        }//: ForEach
+                    }//TabView
+                    .tabViewStyle(PageTabViewStyle())
+                    .frame(height: UIScreen.main.bounds.height/3)
+                    
+                }// : Big Else
+                
+                
+                
+                
+                Spacer()
+                //: ZStack
+            }//: VStack
+            .ignoresSafeArea(edges: .top)
+            .onTapGesture {
+                UIApplication.shared.endEditing()
+            }
+            .navigationBarHidden(true)
+        } //: Nav
+    }
+}
+
 struct MainView: View {
     @EnvironmentObject var mainVM: MainVM
     //Props
@@ -20,94 +65,16 @@ struct MainView: View {
             }
             
             
-            TabBarView()//: VStack
+            TabBarView()
+                .padding(.bottom, (mainVM.edges?.bottom == 0) ? 15 : 0)//: VStack
         
 
         }
     }//: VStack
 }
-struct TabItem: View {
-    @EnvironmentObject var mainVM: MainVM
-    let tab: Tabs
-    var body: some View {
-        VStack {
-            ZStack{
-                RoundedRectTabView()
-                    .frame(width: 45,height: 8)
-                    .foregroundColor(Color.clear)
-                
-                if mainVM.selectedTab == tab {
-                    RoundedRectTabView()
-                        .frame(width: 45,height: 8)
-                    .foregroundColor((mainVM.selectedTab == tab) ? Color("NavBarSelectedColor") : Color("NavBarUnselectedColor"))
-                }// IF Block
-            }
-            Spacer()
-            Button {
-                mainVM.selectedTab = tab
-            } label: {
-                Image(systemName: tab.rawValue)
-                    .imageScale(.large )
-                    .foregroundColor((mainVM.selectedTab == tab) ? Color("NavBarSelectedColor") : Color("NavBarUnselectedColor") )
-                    .frame(height: 30)
-                    .padding(.bottom, 10)
-            }
-
-        }
-    }//: Body
-}
-
-struct TabBarView: View {
-    @EnvironmentObject var mainVM: MainVM
-    var body: some View {
-        VStack(alignment: .center, spacing: 0){
-            Divider()
-                .frame(height: 1.5)
-            //Navigation
-            HStack{
-                ForEach(Tabs.allCases,id:\.self){tab in
-                    Spacer()
-                    TabItem(tab: tab)
-                    Spacer()
-                }
-            }//HStack
-            .padding(.horizontal)
-            .frame(height: 70)
-            .ignoresSafeArea(edges: .bottom)
-            .background(
-                Rectangle().fill(.white))
-            //: ZStack
-            //.frame(height: 70)
-        }
-    }
-}
 
 
-struct HomeView: View {
-    @EnvironmentObject var mainVM: MainVM
-    //Props
-    
-    //Body
-    var body: some View {
-        NavigationView {
-            VStack(alignment: .center, spacing: 0) {
-                MainHeaderView()
-                  //  .frame(height:125)
-                LocationArea()
-                if mainVM.isSearchEditing {
-                    SearchSuggestView().opacity(mainVM.isSearchEditing ? 1 : 0)
-                }
-                Spacer()
-                //: ZStack
-            }//: VStack
-            .ignoresSafeArea(edges: .top)
-            .onTapGesture {
-                UIApplication.shared.endEditing()
-            }
-            .navigationBarHidden(true)
-        } //: Nav
-    }
-}
+
 
 
 
